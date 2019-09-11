525032321bad5803f8f3f43eb2311328caaf3a36
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2019-09-11 10:34:01 +0300

    add utils

---
+++ src/main/java/ru/spor/topjava/graduation/util/ValidationUtil.java
@@ -0,0 +1,56 @@
+package ru.spor.topjava.graduation.util;
+
+import ru.spor.topjava.graduation.HasId;
+import ru.spor.topjava.graduation.util.exception.IllegalRequestDataException;
+import ru.spor.topjava.graduation.util.exception.NotFoundException;
+
+public class ValidationUtil {
+
+    private ValidationUtil() {
+    }
+
+    public static <T> T checkNotFoundWithId(T object, int id) {
+        return checkNotFound(object, "id=" + id);
+    }
+
+    public static void checkNotFoundWithId(boolean found, int id) {
+        checkNotFound(found, "id=" + id);
+    }
+
+    public static <T> T checkNotFound(T object, String msg) {
+        checkNotFound(object != null, msg);
+        return object;
+    }
+
+    public static void checkNotFound(boolean found, String arg) {
+        if (!found) {
+            throw new NotFoundException(arg);
+        }
+    }
+
+    public static void checkNew(HasId bean) {
+        if (!bean.isNew()) {
+            throw new IllegalRequestDataException(bean + " must be new (id=null)");
+        }
+    }
+
+    public static void assureIdConsistent(HasId bean, int id) {
+        //      conservative when you reply, but accept liberally (http://stackoverflow.com/a/32728226/548473)
+        if (bean.isNew()) {
+            bean.setId(id);
+        } else if (bean.getId() != id) {
+            throw new IllegalRequestDataException(bean + " must be with id=" + id);
+        }
+    }
+
+    //  http://stackoverflow.com/a/28565320/548473
+    public static Throwable getRootCause(Throwable t) {
+        Throwable result = t;
+        Throwable cause;
+
+        while (null != (cause = result.getCause()) && (result != cause)) {
+            result = cause;
+        }
+        return result;
+    }
+}
---
+++ src/main/java/ru/spor/topjava/graduation/util/exception/IllegalRequestDataException.java
@@ -0,0 +1,7 @@
+package ru.spor.topjava.graduation.util.exception;
+
+public class IllegalRequestDataException extends RuntimeException {
+    public IllegalRequestDataException(String msg) {
+        super(msg);
+    }
+}
