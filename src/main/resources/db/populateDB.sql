35b5d96f8d91b0eeddddf91eb18b97f1d431c559
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2019-09-11 15:35:00 +0300

    fix DB

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -9,15 +9,15 @@ FROM user_roles;
 DELETE
 FROM users;
 
-ALTER SEQUENCE GLOBAL_SEQ RESTART WITH 1000;
+ALTER SEQUENCE GLOBAL_SEQ RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
-VALUES ('Jason', 'jason@gmail.com', '{noop}admin'),
-       ('Anna', 'anuta@gmail.com', '{noop}user');
+VALUES ('Admin', 'admin@gmail.com', '{noop}admin'),
+       ('User1', 'user1@yandex.ru', '{noop}user');
 
 INSERT INTO user_roles (user_id, role)
-VALUES (1000, 'ROLE_ADMIN'),
-       (1001, 'ROLE_USER');
+VALUES (100000, 'ROLE_ADMIN'),
+       (100001, 'ROLE_USER');
 
 INSERT INTO restaurants (name)
 VALUES ('Restaurant1'),
@@ -25,14 +25,14 @@ VALUES ('Restaurant1'),
        ('Restaurant3');
 
 INSERT INTO dishes (restaurant_id, name, price, date)
-VALUES (1002, 'Lasagna', 155, '2019-08-19'),
-       (1003, 'Salad', 70, '2019-08-19'),
-       (1003, 'Bred', 10, '2019-08-19'),
-       (1004, 'Risotto', 90, '2019-08-18'),
-       (1004, 'Soup', 50, '2019-08-18'),
-       (1004, 'Fish', 115, '2019-08-18');
+VALUES (100002, 'Lasagna', 155, '2019-08-19'),
+       (100003, 'Salad', 70, '2019-08-19'),
+       (100003, 'Bred', 10, '2019-08-19'),
+       (100004, 'Risotto', 90, '2019-08-18'),
+       (100004, 'Soup', 50, '2019-08-18'),
+       (100004, 'Fish', 115, '2019-08-18');
 
 INSERT INTO votes (user_id, restaurant_id, date)
-VALUES (1000, 1003, '2019-08-11'),
-       (1000, 1002, '2019-08-12'),
-       (1001, 1002, '2019-08-11');
+VALUES (100000, 100002, '2019-08-11'),
+       (100000, 100003, '2019-08-12'),
+       (100001, 100004, '2019-08-11');
