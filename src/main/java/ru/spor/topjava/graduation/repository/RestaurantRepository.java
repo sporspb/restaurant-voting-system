47b60ece49a4278c2a4b6e895e8228d9f01c3fd2
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2019-09-13 17:13:29 +0300

    add RestaurantRepository, VoteRepository

---
+++ src/main/java/ru/spor/topjava/graduation/repository/RestaurantRepository.java
@@ -0,0 +1,50 @@
+package ru.spor.topjava.graduation.repository;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.spor.topjava.graduation.model.Restaurant;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
+
+    @Override
+    @Transactional
+    Restaurant save(Restaurant restaurant);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Restaurant r WHERE r.id=:id")
+    int delete(@Param("id") int id);
+
+    @Override
+    Optional<Restaurant> findById(Integer id);
+
+    @Override
+    List<Restaurant> findAll(Sort sort);
+
+    Restaurant findByName(String name);
+
+    @EntityGraph(attributePaths = {"dishes"}, type = EntityGraph.EntityGraphType.LOAD)
+    @Query("SELECT r FROM Restaurant r WHERE r.id=?1")
+    Restaurant getWithDishes(Integer id);
+
+    @EntityGraph(attributePaths = {"votes"}, type = EntityGraph.EntityGraphType.LOAD)
+    @Query("SELECT r FROM Restaurant r WHERE r.id=:id")
+    Restaurant getWithVotes(@Param("id") int id);
+
+    //    @Query("SELECT DISTINCT r FROM Restaurant r LEFT JOIN FETCH r.lunchMenus m WHERE m.actual=:date ORDER BY r.name")
+    @Query("SELECT DISTINCT r FROM Restaurant r WHERE EXISTS (SELECT m FROM Menu m WHERE m.actual=:date) ORDER BY r.name")
+    List<Restaurant> getAllWithMenuForDate(@Param("date") LocalDate date);
+
+}
---
+++ src/main/java/ru/spor/topjava/graduation/repository/VoteRepository.java
@@ -0,0 +1,27 @@
+package ru.spor.topjava.graduation.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.spor.topjava.graduation.model.Vote;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+@Transactional(readOnly = true)
+public interface VoteRepository extends JpaRepository<Vote, Integer> {
+
+    @Override
+    @Transactional
+    Vote save(Vote vote);
+
+    List<Vote> findAllByUserId(int userId);
+
+    List<Vote> findByUserIdAndDateBetween(int userId, @NotNull LocalDate dateFrom, @NotNull LocalDate dateTo);
+
+    List<Vote> findByRestaurantId(int restaurantId);
+
+    List<Vote> findByRestaurantIdAndDateBetween(int restaurantId, LocalDate dateFrom, LocalDate dateTo);
+}
