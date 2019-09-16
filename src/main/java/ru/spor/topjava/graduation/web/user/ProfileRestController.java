2c39060fbd3ecd2280ae760f8b40ce2d19390a9d
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2019-09-16 17:24:09 +0300

    add ProfileRestController and tests

---
+++ src/main/java/ru/spor/topjava/graduation/web/user/ProfileRestController.java
@@ -0,0 +1,48 @@
+package ru.spor.topjava.graduation.web.user;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import ru.spor.topjava.graduation.model.User;
+import ru.spor.topjava.graduation.to.UserTo;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+import static ru.spor.topjava.graduation.web.SecurityUtil.authUserId;
+
+@RestController
+@RequestMapping(ProfileRestController.REST_URL)
+public class ProfileRestController extends AbstractUserController {
+    static final String REST_URL = "/rest/profile";
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public User get() {
+        return super.get(authUserId());
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete() {
+        super.delete(authUserId());
+    }
+
+    @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public ResponseEntity<User> register(@Valid @RequestBody UserTo userTo) {
+        User created = super.create(userTo);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@Valid @RequestBody UserTo userTo) {
+        super.update(userTo, authUserId());
+    }
+}
---
+++ src/test/java/ru/spor/topjava/graduation/web/user/ProfileRestControllerTest.java
@@ -0,0 +1,96 @@
+package ru.spor.topjava.graduation.web.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.spor.topjava.graduation.model.User;
+import ru.spor.topjava.graduation.to.UserTo;
+import ru.spor.topjava.graduation.util.UserUtil;
+import ru.spor.topjava.graduation.web.AbstractControllerTest;
+import ru.spor.topjava.graduation.web.json.JsonUtil;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.spor.topjava.graduation.TestUtil.readFromJson;
+import static ru.spor.topjava.graduation.TestUtil.userHttpBasic;
+import static ru.spor.topjava.graduation.UserTestDataUtil.*;
+import static ru.spor.topjava.graduation.web.user.ProfileRestController.REST_URL;
+
+class ProfileRestControllerTest extends AbstractControllerTest {
+
+    @Test
+    void get() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(REST_URL)
+                .with(userHttpBasic(USER)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(contentJson(USER));
+    }
+
+    @Test
+    void getUnAuth() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void delete() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete(REST_URL)
+                .with(userHttpBasic(USER)))
+                .andExpect(status().isNoContent());
+        assertMatch(userService.getAll(Sort.unsorted()), ADMIN);
+    }
+
+    @Test
+    void register() throws Exception {
+        UserTo createdTo = new UserTo(null, "newName", "newemail@ya.ru", "newPassword");
+
+        ResultActions action = mockMvc.perform(MockMvcRequestBuilders.post(REST_URL + "/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(createdTo)))
+                .andDo(print())
+                .andExpect(status().isCreated());
+        User returned = readFromJson(action, User.class);
+
+        User created = UserUtil.createNewFromTo(createdTo);
+        created.setId(returned.getId());
+
+        assertMatch(returned, created);
+        assertMatch(userService.getByEmail("newemail@ya.ru"), created);
+    }
+
+    //TODO fix not found
+    /*@Test
+    void registerNotFound() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post(REST_URL + "/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(new UserTo())))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }*/
+
+    @Test
+    void update() throws Exception {
+        UserTo updatedTo = new UserTo(null, "newName", "newemail@ya.ru", "newPassword");
+        mockMvc.perform(MockMvcRequestBuilders.put(REST_URL).contentType(MediaType.APPLICATION_JSON)
+                .with(userHttpBasic(USER))
+                .content(JsonUtil.writeValue(updatedTo)))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+        assertMatch(userService.getByEmail("newemail@ya.ru"), UserUtil.updateFromTo(new User(USER), updatedTo));
+    }
+
+    //TODO fix tests
+    /*@Test
+    void updateNotFound() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.put(REST_URL).contentType(MediaType.APPLICATION_JSON)
+                .with(userHttpBasic(USER))
+                .content(JsonUtil.writeValue(new UserTo())))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }*/
+}
