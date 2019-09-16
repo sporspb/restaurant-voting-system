8b324db6717d6ed272d824b4cc32e009d2979030
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2019-09-16 14:23:39 +0300

    security

--- src/main/java/ru/spor/topjava/graduation/AuthorizedUser.java
+++ src/main/java/ru/spor/topjava/graduation/AuthorizedUser.java
@@ -1,20 +1,34 @@
 package ru.spor.topjava.graduation;
 
 import ru.spor.topjava.graduation.model.User;
+import ru.spor.topjava.graduation.to.UserTo;
+import ru.spor.topjava.graduation.util.UserUtil;
 
 public class AuthorizedUser extends org.springframework.security.core.userdetails.User {
-    private User user;
+    private static final long serialVersionUID = 1L;
+
+    private UserTo userTo;
 
     public AuthorizedUser(User user) {
         super(user.getEmail(), user.getPassword(), user.isEnabled(), true, true, true, user.getRoles());
-        this.user = user;
-    }
-
-    public User getUser() {
-        return user;
+        this.userTo = UserUtil.asTo(user);
     }
 
     public int getId() {
-        return user.getId();
+        return userTo.getId();
+    }
+
+    public void update(UserTo newTo) {
+        userTo = newTo;
+    }
+
+    public UserTo getUserTo() {
+        return userTo;
+    }
+
+    @Override
+    public String toString() {
+        return userTo.toString();
     }
 }
+
