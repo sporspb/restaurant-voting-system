c0c94d8bc1fb4cc69daebd9dd0eb4d3c4ae5b8e2
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2019-09-16 14:18:27 +0300

    add Exception Handle

---
+++ src/main/java/ru/spor/topjava/graduation/util/exception/ErrorType.java
@@ -0,0 +1,8 @@
+package ru.spor.topjava.graduation.util.exception;
+
+public enum ErrorType {
+    APP_ERROR,
+    DATA_NOT_FOUND,
+    DATA_ERROR,
+    VALIDATION_ERROR
+}
---
+++ src/main/java/ru/spor/topjava/graduation/web/ExceptionInfoHandler.java
@@ -0,0 +1,80 @@
+package ru.spor.topjava.graduation.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import ru.spor.topjava.graduation.util.ValidationUtil;
+import ru.spor.topjava.graduation.util.exception.ErrorInfo;
+import ru.spor.topjava.graduation.util.exception.ErrorType;
+import ru.spor.topjava.graduation.util.exception.IllegalRequestDataException;
+import ru.spor.topjava.graduation.util.exception.NotFoundException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ValidationException;
+
+import static org.springframework.context.i18n.LocaleContextHolder.getLocale;
+import static ru.spor.topjava.graduation.util.exception.ErrorType.*;
+
+@RestControllerAdvice(annotations = RestController.class)
+@Order(Ordered.HIGHEST_PRECEDENCE + 5)
+public class ExceptionInfoHandler {
+    private static Logger log = LoggerFactory.getLogger(ExceptionInfoHandler.class);
+
+    private final ReloadableResourceBundleMessageSource bundleMessageSource;
+
+    public ExceptionInfoHandler(ReloadableResourceBundleMessageSource bundleMessageSource) {
+        this.bundleMessageSource = bundleMessageSource;
+    }
+
+    //    https://stackoverflow.com/questions/538870/should-private-helper-methods-be-static-if-they-can-be-static
+    private static ErrorInfo logAndGetErrorInfo(HttpServletRequest req, Exception e, boolean logException, ErrorType errorType) {
+        Throwable rootCause = ValidationUtil.getRootCause(e);
+        if (logException) {
+            log.error(errorType + " at request " + req.getRequestURL(), rootCause);
+        } else {
+            log.warn("{} at request  {}: {}", errorType, req.getRequestURL(), rootCause.toString());
+        }
+        return new ErrorInfo(req.getRequestURL(), errorType, rootCause.toString());
+    }
+
+    //  http://stackoverflow.com/a/22358422/548473
+    @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
+    @ExceptionHandler(NotFoundException.class)
+    public ErrorInfo handleError(HttpServletRequest req, NotFoundException e) {
+        return logAndGetErrorInfo(req, e, false, DATA_NOT_FOUND);
+    }
+
+    @ResponseStatus(value = HttpStatus.CONFLICT)  // 409
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ErrorInfo conflict(HttpServletRequest req, DataIntegrityViolationException e) {
+        Throwable rootCause = ValidationUtil.getRootCause(e);
+        log.error(DATA_ERROR + " at request " + req.getRequestURL(), rootCause);
+        if (rootCause.toString().contains("users_unique_email_idx")) {
+            return new ErrorInfo(req.getRequestURL(), DATA_ERROR, bundleMessageSource.getMessage("valid.userForm.dublicateMail", null, getLocale()));
+        }
+        return new ErrorInfo(req.getRequestURL(), DATA_ERROR, rootCause.toString());
+    }
+
+    @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)  // 422
+    @ExceptionHandler({IllegalRequestDataException.class, MethodArgumentTypeMismatchException.class, HttpMessageNotReadableException.class, ValidationException.class, MethodArgumentNotValidException.class})
+    public ErrorInfo illegalRequestDataError(HttpServletRequest req, Exception e) {
+        return logAndGetErrorInfo(req, e, false, VALIDATION_ERROR);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public ErrorInfo handleError(HttpServletRequest req, Exception e) {
+        return logAndGetErrorInfo(req, e, true, APP_ERROR);
+    }
+}
